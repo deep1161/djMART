741716d4db3222fb8e9960ae530632d557d84084
deep1161 <93637913+deep1161@users.noreply.github.com>
2023-12-11 15:18:03 +0530

    Done2

--- client/src/components/Prices.js
+++ client/src/components/Prices.js
@@ -1,32 +1,32 @@
 export const Prices = [
   {
     _id: 0,
-    name: "₹0 to 1999",
-    array: [0, 1999],
+    name: "$0 to 19",
+    array: [0, 19],
   },
   {
     _id: 1,
-    name: "₹2000 to 3999",
-    array: [2000, 3999],
+    name: "$20 to 39",
+    array: [20, 39],
   },
   {
     _id: 2,
-    name: "₹4000 to 5999",
-    array: [4000, 5999],
+    name: "$40 to 59",
+    array: [40, 59],
   },
   {
     _id: 3,
-    name: "₹6000 to 7999",
-    array: [6000, 7999],
+    name: "$60 to 79",
+    array: [60, 79],
   },
   {
     _id: 4,
-    name: "₹8000 to 9999",
-    array: [8000, 9999],
+    name: "$80 to 99",
+    array: [80, 99],
   },
   {
     _id: 4,
-    name: "₹10000 or more",
-    array: [10000, 999999],
+    name: "$100 or more",
+    array: [100, 9999],
   },
 ];
